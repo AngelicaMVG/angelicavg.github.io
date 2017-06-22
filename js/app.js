27d7f04b79dba81358d4f8c49a4ecb08589c5ed7
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-06-22 10:13:28 -0500

    nuevo repositorio

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.button-collapse').sideNav({
+     menuWidth: 500, // Default is 300
+     edge: 'left', // Choose the horizontal origin
+     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+     draggable: true // Choose whether you can drag to open on touch screens
+   });
+   $('.parallax').parallax();
+   $('.modal').modal();
+
+ });
+
+var cargarProgressBar = function(){
+    var scroll = document.body.scrollTop;
+    if(scroll >= 750){
+      $('.progress').show('slow');
+    } else if(scroll <700){
+      $('.progress').hide();
+    }
+ }
+
+document.addEventListener("scroll", cargarProgressBar);
